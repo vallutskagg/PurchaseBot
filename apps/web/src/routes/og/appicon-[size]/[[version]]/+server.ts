b6130588f026ea5270c35d95051cc9b70b76c29d
Valtteri Ranta <e2102956@vamk.fi>
2026-04-19 03:30:34 +0300

    fix(web): make node build work on self-host

--- apps/web/src/routes/og/appicon-[size]/[[version]]/+server.ts
+++ apps/web/src/routes/og/appicon-[size]/[[version]]/+server.ts
@@ -1,27 +1,5 @@
 import type { RequestHandler } from './$types';
-import AppIcon from '$lib/components/og/AppIcon.svelte';
-import { svelteOg } from '$lib/utils/svelteOg';
-import { type } from 'arktype';
-import { ogCatchHandler } from '../../ogCatchHandler';
 
-const sizeType = type(['parsedNumber', '|>', type('1 <= integer <= 512')]);
-
-export const GET: RequestHandler = async ({ params }) => {
-	try {
-		const size = sizeType.assert(params.size);
-		return await svelteOg(
-			{
-				component: AppIcon,
-				props: {
-					size
-				}
-			},
-			{
-				width: size,
-				height: size
-			}
-		);
-	} catch (e) {
-		return ogCatchHandler(e);
-	}
+export const GET: RequestHandler = async () => {
+	return new Response('Self-hosted OG generation is disabled.', { status: 404 });
 };
--- apps/web/src/routes/og/splash-[width]x[height]/[[version]]/+server.ts
+++ apps/web/src/routes/og/splash-[width]x[height]/[[version]]/+server.ts
@@ -1,23 +1,5 @@
 import type { RequestHandler } from './$types';
-import Splash from '$lib/components/og/Splash.svelte';
-import { svelteOg } from '$lib/utils/svelteOg';
-import { type } from 'arktype';
-import { ogCatchHandler } from '../../ogCatchHandler';
 
-const boundedSize = type('1 <= integer <= 6000');
-const imageParams = type({
-	width: ['parsedInteger', '|>', boundedSize],
-	height: ['parsedInteger', '|>', boundedSize]
-});
-
-export const GET: RequestHandler = async ({ params }) => {
-	try {
-		const { width, height } = imageParams.assert(params);
-		return await svelteOg(Splash, {
-			width,
-			height
-		});
-	} catch (e) {
-		return ogCatchHandler(e);
-	}
+export const GET: RequestHandler = async () => {
+	return new Response('Self-hosted OG generation is disabled.', { status: 404 });
 };
